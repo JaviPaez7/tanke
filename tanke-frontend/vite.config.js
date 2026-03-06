8f704b1d9d62f0f515881ca4e37399183c91e864
JaviPaez7 <javipaeztoledo@gmail.com>
2026-03-06 10:09:47 +0000

    feat: implementar PWA

--- tanke-frontend/vite.config.js
+++ tanke-frontend/vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Tanke - Ahorra en Gasolina',
+        short_name: 'Tanke',
+        description: 'Buscador de gasolineras baratas en tiempo real',
+        theme_color: '#0f172a', // El azul oscuro de tu cabecera
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
